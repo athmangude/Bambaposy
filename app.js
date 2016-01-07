fef5249fa8d50c8cc914c946bacfcc3aa38219a1
Athman Gude <athmangude@gmail.com>
2016-01-07 11:27:11 +0300

    getting started with logging the user in

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var API_URL = 'http://private-anon-f6d541990-bambapos.apiary-mock.com/';
     $('form[name="sign-in-form"] input')
         .on('focus', function() {
             $(this).siblings('small').removeClass('hidden');
@@ -10,9 +11,28 @@ $(document).ready(function() {
 
     $('form[name="sign-in-form"]').parsley()
         .on('form:submit', function(event) {
-            console.log(event);
             var email = event.fields[0].value;
             var password = event.fields[1].value;
+
+            fetch(API_URL+'signin', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    channel: "LOGIN"
+                })
+            })
+            .then(function(response) {
+                return response.json();
+            }).then(function(responseData) {
+                console.log(responseData);
+            }).catch(function(error) {
+                console.log(error);
+            });
 
             console.log(email, password);
         });
