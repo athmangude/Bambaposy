9cc33a43956d2df142f3815f508a3cb70018d124
Athman Gude <athmangude@gmail.com>
2016-01-07 11:06:18 +0300

    picking up form data when submitted

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
     $('form[name="sign-in-form"] input')
         .on('focus', function() {
-            console.log($(this).siblings('small'));
             $(this).siblings('small').removeClass('hidden');
         })
         .on('blur', function() {
             $(this).siblings('small').addClass('hidden');
+        });
+
+
+    $('form[name="sign-in-form"]').parsley()
+        .on('form:submit', function(event) {
+            console.log(event);
+            var email = event.fields[0].value;
+            var password = event.fields[1].value;
+
+            console.log(email, password);
         });
 });
