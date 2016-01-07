0b694e642b8bd12c9bfa1c8586fff9433a4feb83
Athman Gude <athmangude@gmail.com>
2016-01-07 14:40:49 +0300

    refactored form submission handling logic

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ $(document).ready(function() {
         });
 
 
-    $('form[name="sign-in-form"]').parsley()
-        .on('form:submit', function(event) {
-            var email = event.fields[0].value;
-            var password = event.fields[1].value;
+    $('form[name="sign-in-form"]')
+        .on('submit', function(event) {
+            event.preventDefault();
+
+            console.log(event);
+
+            var self = $(this);
+
+            self.parsley();
+
+            console.log(self.parsley());
+
+            var email = self.parsley().fields[0].value;
+            var password = self.parsley().fields[1].value;
 
             // disable button
             $('#sign-in-button').attr('disabled', 'disabled');
 
             // show progress indicator
             $('div.sign-in-button-holder i').removeClass('hidden');
+
+            console.log('initiating api request');
 
             fetch(API_URL+'signin', {
                 method: 'post',
@@ -41,10 +53,27 @@ $(document).ready(function() {
                 // hide progress indicator
                 $('div.sign-in-button-holder i').addClass('hidden');
                 console.log(responseData);
+
+                // $('form[name="create-store-form-stage-one"]').addClass('hidden');
+                // $('form[name="create-store-form-stage-two"]').addClass('hidden fadeIn');
+
             }).catch(function(error) {
+                console.log('in error block');
                 console.log(error);
             });
+        });
 
-            console.log(email, password);
+
+    $('form[name="create-store-form-stage-one"]')
+        .on('submit', function(event) {
+            event.preventDefault();
+
+            var self = $(this);
+
+            var storeName = self.parsley().fields[0].value;
+            var email = self.parsley().fields[1].value;
+            var password = self.parsley().fields[2].value;
+
+            console.log(storeName, email, password);
         });
 });
